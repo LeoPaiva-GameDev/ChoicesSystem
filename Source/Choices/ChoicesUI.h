a8f79698e4501824683f7a8e68a9a1d1297e49c8
LeoPaiva-GameDev <leonardopaivagamedev@gmail.com>
2020-09-13 16:44:34 -0300

    UI
    Include choices UI files

---
+++ Source/Choices/ChoicesUI.h
@@ -0,0 +1,17 @@
+// Code by Leonardo Paiva
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ChoicesUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHOICES_API UChoicesUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
