3edd8ec4be9498addc513a70a2ff2152504c53f5
LeoPaiva-GameDev <70583559+LeoPaiva-GameDev@users.noreply.github.com>
2020-11-21 17:19:13 -0300

    Not finished yet

--- Source/Choices/Subtitle.h
+++ Source/Choices/Subtitle.h
@@ -12,11 +12,9 @@ struct FSubtitle : public FTableRowBase
 {
 	GENERATED_USTRUCT_BODY()
 
-    /*The subtitle that will be displayed for a specific period of time in our UI*/
     UPROPERTY(EditAnywhere)
 	FString Subtitle;
 
-	/*The relative time in seconds, that the subtitle will appear*/
 	UPROPERTY(EditAnywhere)
 	float AssociatedTime;
 };
