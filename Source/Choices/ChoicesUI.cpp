51bb2a59863553014760d77c0ba84818ff0c5fb3
LeoPaiva-GameDev <70583559+LeoPaiva-GameDev@users.noreply.github.com>
2020-11-21 17:14:14 -0300

    Not finished yet

--- Source/Choices/ChoicesUI.cpp
+++ Source/Choices/ChoicesUI.cpp
@@ -7,6 +7,5 @@ void UChoicesUI::UpdateSubtitles(TArray<FSubtitle> Subtitles)
 {
     if (!Subtitles.IsValidIndex(0)) return;
  
-    //Start a background task in a low priority thread
     (new FAutoDeleteAsyncTask<UMGAsyncTask>(Subtitles, this))->StartBackgroundTask();
 }
